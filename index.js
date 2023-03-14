ee9d44abfd8e5fd0f1abb73e9a5547204e2a743e
SrijanTimsina <srijan.timsina.39@gmail.com>
2023-03-14 15:14:26 +0545

    first

--- index.js
+++ index.js
@@ -26,9 +26,36 @@ const bestTokens = document.getElementById("best-token");
 const analyticsBtn = document.getElementById("analytics-btn");
 const analyticsContainer = document.getElementById("analytics");
 
+function scrollToSmoothly(pos, time) {
+	var currentPos = window.pageYOffset;
+	var start = null;
+	if (time == null) time = 500;
+	(pos = +pos), (time = +time);
+	window.requestAnimationFrame(function step(currentTime) {
+		start = !start ? currentTime : start;
+		var progress = currentTime - start;
+		if (currentPos < pos) {
+			window.scrollTo(
+				0,
+				((pos - currentPos) * progress) / time + currentPos
+			);
+		} else {
+			window.scrollTo(
+				0,
+				currentPos - ((currentPos - pos) * progress) / time
+			);
+		}
+		if (progress < time) {
+			window.requestAnimationFrame(step);
+		} else {
+			window.scrollTo(0, pos);
+		}
+	});
+}
 analyticsBtn.addEventListener("click", () => {
 	analyticsContainer.classList.add("show");
-	analyticsContainer.scrollIntoView();
+	var clientHeight = document.getElementById("home").clientHeight;
+	scrollToSmoothly(clientHeight, 600);
 });
 
 const barArray = [
