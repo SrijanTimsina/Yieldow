8f28e5c16f5210b2f06b4cb24805cfd8ec36f915
SrijanTimsina <srijan.timsina.39@gmail.com>
2023-03-17 01:04:23 +0545

    additional changes

--- swap/index.js
+++ swap/index.js
@@ -9,3 +9,60 @@ document.getElementById("close").addEventListener("click", () => {
 	sidebarContainer.classList.remove("side-bar-show");
 	sidebarContainer.classList.add("side-bar-hide");
 });
+
+const tableData = document.querySelectorAll(".table-data");
+const dataRow = document.querySelectorAll(".table-data-row");
+const mobileTable = document.querySelectorAll(".mob-scr-tbl");
+const mobileData = document.querySelectorAll(".mob-scr-tbl-data");
+const popupButton = document.querySelectorAll(".popup-button");
+const container = document.getElementById("container");
+const closePopup = document.getElementById("popup-close");
+
+const tableOptionsToggle = (el, parent) => {
+	let parentNode = el.parentNode;
+	if (parentNode.classList.contains("show")) {
+		parentNode.classList.remove("show");
+		return;
+	}
+	parent.forEach((el) => el.classList.remove("show"));
+	parentNode.classList.add("show");
+};
+mobileData.forEach((el) =>
+	el.addEventListener("click", () => {
+		tableOptionsToggle(el, mobileTable);
+	})
+);
+dataRow.forEach((el) =>
+	el.addEventListener("click", () => {
+		tableOptionsToggle(el, tableData);
+	})
+);
+
+// mobileData.forEach((el) => {
+// 	el.addEventListener("click", () => {
+// 		let parentNode = el.parentNode;
+// 		if (parentNode.classList.contains("show")) {
+// 			parentNode.classList.remove("show");
+// 			return;
+// 		}
+// 		mobileTable.forEach((el) => el.classList.remove("show"));
+// 		parentNode.classList.add("show");
+// 	});
+// });
+// dataRow.forEach((el) => {
+// 	el.addEventListener("click", () => {
+// 		const parentNode = el.parentNode;
+// 		if (parentNode.classList.contains("show")) {
+// 			parentNode.classList.remove("show");
+// 			return;
+// 		}
+// 		tableData.forEach((el) => el.classList.remove("show"));
+// 		parentNode.classList.add("show");
+// 	});
+// });
+
+const popupToggle = () => container.classList.toggle("popup-active");
+closePopup.addEventListener("click", popupToggle);
+popupButton.forEach((el) => {
+	el.addEventListener("click", popupToggle);
+});
